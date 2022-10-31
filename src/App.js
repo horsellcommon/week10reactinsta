0fd95797846cd396204efa7c664ae1b507aca1fc
horsellcommon <crawfordj716@gmail.com>
2022-10-31 10:56:53 +0000

    props, useStates and component importation into App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import "./App.css";
+import React from "react";
+import Box from "./components/box";
+import { useState } from "react";
 
 function App() {
-  
+  const [user, setUser] = useState("Cristo Redentor");
+  const [age, setAge] = useState(90);
   return (
     <div className="App">
       <h1>Look at this great header</h1>
+      <Box name="Big Ben" age="163" />
+      <Box name="Machu Picchu" age="572" />
+      <Box name="Burj Khalifa" age="13" />
+      <Box name={user} age={age} />
     </div>
   );
 }
---
+++ src/components/box.js
@@ -0,0 +1,11 @@
+const Box = ({ name, age }) => {
+  return (
+    <div>
+      <h1>
+        {name}, {age} years old.
+      </h1>
+    </div>
+  );
+};
+
+export default Box;
