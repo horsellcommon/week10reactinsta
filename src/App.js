8c457e13b4cdd07a3d4b125df9dddf249055c228
horsellcommon <crawfordj716@gmail.com>
2022-10-31 11:37:06 +0000

    Added ternary operators

--- src/App.js
+++ src/App.js
@@ -4,15 +4,26 @@ import Box from "./components/box";
 import { useState } from "react";
 
 function App() {
-  const [user, setUser] = useState("Cristo Redentor");
-  const [age, setAge] = useState(90);
+  const [user, setUser] = useState();
+  const [age, setAge] = useState();
   return (
     <div className="App">
-      <h1>Look at this great header</h1>
-      <Box name="Big Ben" age="163" />
-      <Box name="Machu Picchu" age="572" />
-      <Box name="Burj Khalifa" age="13" />
-      <Box name={user} age={age} />
+      <input onChange={(e) => setUser(e.target.value)} />
+      <input onChange={(e) => setAge(e.target.value)} />
+
+      {user ? (
+        <div>
+          <h1>Look at this great header</h1>
+          <Box name="Big Ben" age="163" />
+          <Box name="Machu Picchu" age="572" />
+          <Box name="Burj Khalifa" age="13" />
+          <Box name={user} age={age} />
+        </div>
+      ) : (
+        <div>
+          <h1>This is the else part of the statement</h1>
+        </div>
+      )}
     </div>
   );
 }
